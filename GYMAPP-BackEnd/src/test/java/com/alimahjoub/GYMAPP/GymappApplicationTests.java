a49138d76a6e1156d67e88542401b8ed3bb4e56b
Ali Mahjoub <alimahjoub047@gmail.com>
2024-11-28 14:34:19 +0100

    first commit

---
+++ GYMAPP-BackEnd/src/test/java/com/alimahjoub/GYMAPP/GymappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alimahjoub.GYMAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
